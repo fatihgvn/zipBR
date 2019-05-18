22a1cd132b1a19187d79020ce5a65920aa4af492
Fatih Güven <31927632+fatihgvn@users.noreply.github.com>
2019-05-18 16:33:20 +0300

    Update zipBR.py
    add multi core

--- zipBR.py
+++ zipBR.py
@@ -4,7 +4,7 @@
 import argparse, time, os, datetime
 from lib import keygenerator
 from lib import zip
-from threading import Thread
+from multiprocessing.pool import ThreadPool
 
 
 def version():
@@ -26,6 +26,7 @@ parser.add_argument("-n","--useNumbers", help="Use all figures", action="store_t
 parser.add_argument("-s","--useSpecialChars", help="Use special characters", action="store_true")
 parser.add_argument("-p","--useSpace", help="Use <SPACE> character", action="store_true")
 parser.add_argument("-v","--version", help="Show version number", action='store_true')
+parser.add_argument("--core", help="Maximum number of cores to use", default=1, type=int)
 
 args = parser.parse_args()
 
@@ -65,16 +66,25 @@ if __name__ == "__main__":
         startTime = datetime.datetime.now()
 
         print ("Start Time %s" % str(startTime))
+
+
+        pool = ThreadPool(processes=args.core)
+
         while True:
             key = generator.next(key)
+            
+            async_result = pool.apply_async(zip.check, (args.file, key, path))
 
             if keylen != len(key):
                 keylen = len(key)
                 print("Trying a %d-digit password" % keylen)
 
-            if zip.check(args.file, key, path):
+            return_val = async_result.get()
+
+            if return_val == True:
                 print("password is %s" % key)
                 break
+                    
 
 
         endTime = datetime.datetime.now()
